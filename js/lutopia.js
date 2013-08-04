effcc110f0f75fa1ecc275f3a89cbadb0b3f264a
Dan Burzo <danburzo@gmail.com>
2013-08-04 12:16:13 +0300

    Added more pages, adjusted layout

--- js/lutopia.js
+++ js/lutopia.js
@@ -1,31 +1,6 @@
-var Lu = {
-	goTo: function(route) {
-		$("#" + route + "-section").show().siblings('section').hide();
-	}
-};
-
-Lu.Router = Backbone.Router.extend({
-	routes: {
-		'story': 'goo',
-		'gallery': 'goo',
-
-		goo: function(route) {
-			console.log(route);
-			Lu.goTo(route);
-		}
-	}
-});
-
-Lu.router = new Lu.Router();
-
-Backbone.history.start();
+var Lu = {};
 
 $(function() {
-	$('nav a').on('click', function() {
-		var route = $(this).attr('href').split('#')[1];
-		Lu.router.navigate(route, true);
-		return false;
-	});
 
 	brush.init({
 		canvas: $("#artboard")[0],
